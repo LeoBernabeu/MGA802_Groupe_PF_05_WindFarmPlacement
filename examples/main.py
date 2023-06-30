1e5e2244cf23103dde27632c3d2c32fdca2ff166
LeoBernabeu <leobernabeu01@gmail.com>
2023-07-01 00:48:05 +0200

    Update main.py

--- examples/main.py
+++ examples/main.py
@@ -20,48 +20,43 @@ if __name__ == '__main__':
     with open(YAML_filename, 'r') as file:
         parameters = yaml.load(file, Loader=yaml.FullLoader)
 
+    print_message_console("Parameters have been acquired successfully")
+
     # Reference file
     path_to_data = parameters['path_to_data']
     reference_file = parameters['reference_file']
 
     # Study area and data acquisition parameters definition
-    lon_min = parameters['lon_min']                     # Min longitude of our study region
-    lon_max = parameters['lon_max']                     # Max longitude of our study region
-    lat_min = parameters['lat_min']                     # Min latidute of our study region
-    lat_max = parameters['lat_max']                     # Max latidute of our study region
-    precision_lon = int(parameters['precision_lon'])    # Number of points to study along the longitude of our study region
-    precision_lat = int(parameters['precision_lat'])    # Number of points to study along the latitude of our study region
-    study_alt = parameters['study_alt']                 # Altitude at which we gather wind data in meters, can be either 10, 30 or 50
-    study_years = parameters['study_years']             # List of years that we want to study
-    activate_multi_process = parameters['activate_multi_process']   # Activate multi-thread processing to decrease calculation time
-    num_areas_interest = int(parameters['num_areas_interest'])      # Number of sub-areas of intesrests to further study within the study area
+    lon_min = parameters['lon_min']  # Min longitude of our study region
+    lon_max = parameters['lon_max']  # Max longitude of our study region
+    lat_min = parameters['lat_min']  # Min latidute of our study region
+    lat_max = parameters['lat_max']  # Max latidute of our study region
+    precision_lon = int(parameters['precision_lon'])  # Number of points to study along the longitude of our study region
+    precision_lat = int(parameters['precision_lat'])  # Number of points to study along the latitude of our study region
 
-    # Elevation data
-    precision_lon_elevation = int(parameters['precision_lon_elevation'])
-    precision_lat_elevation = int(parameters['precision_lat_elevation'])
-
-    # Wind farm turbine parameters
-    target_power = parameters['target_power']   # Target power of the wind turbine farm in Watts
-    num_windmills = parameters['num_windmills'] # Number of wind turbines in the farm
-    turb_height = parameters['turb_height']     # Altitude of turbine in meters, for accurate results, should be equal to study_alt (either 10, 30 or 50)
-    blade_length = parameters['blade_length']   # Length of turbine blades in meters
-    cut_in_speed = parameters['cut_in_speed']   # Speed at which the turbine starts producing power
-    cut_out_speed = parameters['cut_out_speed']  # Speed at which the turbine stops producing power
-    turbine_spacing = parameters['turbine_spacing']  # Spacing of individual turbines in the farm, a spacing of 5 will space them by 5 times their turbine diameter
-    print_message_console("Parameters have been acquired successfully")
-
-    # Define study area and get wind history data
+    """On définit la zone d'étude avec les paramètres précédents. On distingue deux cas, celui où l'utilisateur passe un
+    chemin vers le dossier de données, et celui-ci où il n'a pas de précision et donc on considère qu'il utilise le
+    dossier data présent à la racine du projet."""
     if path_to_data:
         study_area = WindFarmPlacement(lon_min, lon_max, lat_min, lat_max, precision_lat, precision_lon, path_to_data)
     else:
         study_area = WindFarmPlacement(lon_min, lon_max, lat_min, lat_max, precision_lat, precision_lon)
 
+    study_alt = parameters['study_alt']  # Altitude at which we gather wind data in meters, can be either 10, 30 or 50
+    study_years = parameters['study_years']  # List of years that we want to study
+    activate_multi_process = parameters['activate_multi_process']  # Activate multi-thread processing to decrease calculation time
+
+    """On effectue ensuite le traitement des données pour les années passées en paramètre. On distingue la encore
+    plusieurs cas, selon que l'utilisateur a activé la totalité du parallélisme ou non, et aussi selon qu'il a fournit
+    un fichier de référence pour les données différent de celui de Climate Weather Canada"""
     if activate_multi_process:
+        # Parallélisme sur les années et sur les mois
         if reference_file:
             study_area.get_wind_history_data_full_threaded(study_years, study_alt, reference_file=reference_file)
         else:
             study_area.get_wind_history_data_full_threaded(study_years, study_alt)
     else:
+        # Parallélisme sur les mois
         if reference_file:
             study_area.get_wind_history_data(study_years, study_alt, reference_file=reference_file)
         else:
@@ -81,15 +76,42 @@ if __name__ == '__main__':
     plt.savefig(figure_path)
 
     plt.show()
+
+    # Wind farm turbine parameters
+    target_power = parameters['target_power']  # Target power of the wind turbine farm in Watts
+    num_windmills = parameters['num_windmills']  # Number of wind turbines in the farm
+    turb_height = parameters['turb_height']  # Altitude of turbine in meters, for accurate results, should be equal to study_alt (either 10, 30 or 50)
+    blade_length = parameters['blade_length']  # Length of turbine blades in meters
+    cut_in_speed = parameters['cut_in_speed']  # Speed at which the turbine starts producing power
+    cut_out_speed = parameters['cut_out_speed']  # Speed at which the turbine stops producing power
+    turbine_spacing = parameters['turbine_spacing']  # Spacing of individual turbines in the farm, a spacing of 5 will space them by 5 times their turbine diameter
     
     # Create wind farm object and add wind turbines to the wind farm
     wind_farm = WindFarm(target_power)
     for i in range(num_windmills):
+        # On crée les éoliennes du champ
         wind_farm.add_windmill(Windmill(turb_height, blade_length, cut_in_speed, cut_out_speed))
+
+    """On peut si l'on souhaite ajouter d'autres éoliennes avec des paramètres différents.
+    Par exemple : wind_farm.add_windmill(Windmill(turb_height, 30, 5))
+    Le champ d'éolienne utilisera alors les éoliennes les plus grandes (et donc plus puissantes) pour déterminer
+    les espacements."""
+
     print_message_console("Wind turbine farm has been created successfully")
 
+    """L'utilisateur peut indiquer le nombre de parcelles convenables à lui retourner. Selon, la précision et du maillage
+    et la puissance visé, de très nombreuses parcelles de la zone d'étude peuvent convenir. Pour éviter de perdre
+    l'utilisateur avec un trop grand nombre de résultats, nous envoyons par défaut seulement les 5 meilleures zones.
+    L'utilisateur peut en demander plus ou moins dans le fichier ed paramètres."""
+    num_areas_interest = int(parameters['num_areas_interest'])
+
+
     # Calculate the power matrix produced by our wind farm within the study area and find an area of interest
-    areas_of_interest, power_matrix = study_area.find_adapted_zone(wind_farm, turbine_spacing, nb_area=num_areas_interest)
+    if num_areas_interest:
+        areas_of_interest, power_matrix = study_area.find_adapted_zone(wind_farm, turbine_spacing, nb_area=num_areas_interest)
+    else:
+        # Si on utilise la valeur par défaut
+        areas_of_interest, power_matrix = study_area.find_adapted_zone(wind_farm, turbine_spacing)
     print_message_console("Power matrix and areas of interests have been calculated successfully")
 
     # Print and save power estimation figure
@@ -103,6 +125,10 @@ if __name__ == '__main__':
     plt.savefig(figure_path)
 
     plt.show()
+
+    # Elevation data
+    precision_lon_elevation = int(parameters['precision_lon_elevation'])
+    precision_lat_elevation = int(parameters['precision_lat_elevation'])
 
     # Retrieve elevation data for all areas of interest and calculate topography score
     print_message_console("About to retrieve topography from Open Elevation API, this might take a few minutes.")
